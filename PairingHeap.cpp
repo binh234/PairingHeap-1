9fbbfb23af5d28dd33521bf771f5466e8763bf2b
Cuong Nguyen Dang <cuong.nguyen1605@hcmut.edu.vn>
2020-01-15 15:45:30 +0700

    def class Node, class PairingHeap

--- PairingHeap.cpp
+++ PairingHeap.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
 using namespace std;
-int main(){
+class Node{
+    public:
+    int value;
+    Node* left;  //first child
+    Node* right; //next sibling or parent
+    
+    Node(int value){
+        this->value = value;
+        this->left = nullptr;
+        this->right = nullptr;
+    }
+    Node(){
+        this->left = nullptr;
+        this->right = nullptr;
+    }
+};
 
+class PairingHeap{
+    private: Node *root;
+    public:
+        PairingHeap(){
+            root = new Node();
+        }
+        void FindMin();
+        void Insert(int x);
+        void Meld(PairingHeap h);
+        void DecreaseKey(int delta, int key);
+        void Delete(int key);
+        void DeleteMin();
+};
+
+PairingHeap MakeHeap(){
+    return *(new PairingHeap());
+}
+
+int main(){
+    PairingHeap h = MakeHeap();
     return 0;
 }
